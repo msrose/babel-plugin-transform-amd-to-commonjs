98b979f1066309c9655000c29d9cca962c3351b4
Michael Rose <michael_rose@outlook.com>
2016-12-10 21:19:53 -0800

    Refactor handling of define block arguments

--- index.js
+++ index.js
@@ -1,47 +1,56 @@
-module.exports = ({ types: t }) => ({
-  visitor: {
-    ExpressionStatement(path) {
-      const { node, parent } = path;
-      if(!t.isProgram(parent) || !t.isCallExpression(node.expression)) return;
-      const { name } = node.expression.callee;
-      if(name !== 'define' && name !== 'require') return;
-      const [dependencyList] = node.expression.arguments;
-      let [, factory] = node.expression.arguments;
-      if(name === 'require' && !t.isArrayExpression(dependencyList)) {
-        return;
-      }
-      if(t.isArrayExpression(dependencyList)) {
-        dependencyList.elements.forEach((el, i) => {
-          const paramName = factory.params[i];
-          const requireCall = t.callExpression(
-            t.identifier('require'),
-            [t.stringLiteral(el.value)]
-          );
-          if(paramName) {
-            path.insertBefore(t.variableDeclaration('var', [
-              t.variableDeclarator(paramName, requireCall)
-            ]));
-          } else {
-            path.insertBefore(t.expressionStatement(requireCall));
-          }
-        });
-      } else if(t.isFunctionExpression(dependencyList)) {
-        factory = dependencyList;
-      }
-      const factoryReplacement = t.callExpression(t.functionExpression(null, [], factory.body), []);
-      if(name === 'define') {
-        path.replaceWith(
-          t.expressionStatement(
-            t.assignmentExpression(
-              '=',
-              t.memberExpression(t.identifier('module'), t.identifier('exports')),
-              factoryReplacement
+module.exports = ({ types: t }) => {
+  const decodeDefineArguments = ([dependencyList, factory]) => {
+    if(t.isFunctionExpression(dependencyList)) {
+      return { factory: dependencyList };
+    }
+    return { dependencyList, factory };
+  };
+
+  return {
+    visitor: {
+      ExpressionStatement(path) {
+        const { node, parent } = path;
+        if(!t.isProgram(parent) || !t.isCallExpression(node.expression)) return;
+
+        const { name } = node.expression.callee;
+        if(name !== 'define' && name !== 'require') return;
+
+        const defineArgs = decodeDefineArguments(node.expression.arguments);
+        const { dependencyList, factory } = defineArgs;
+        if(name === 'require' && !t.isArrayExpression(dependencyList)) return;
+
+        if(dependencyList) {
+          dependencyList.elements.forEach((el, i) => {
+            const paramName = factory.params[i];
+            const requireCall = t.callExpression(
+              t.identifier('require'),
+              [t.stringLiteral(el.value)]
+            );
+            if(paramName) {
+              path.insertBefore(t.variableDeclaration('var', [
+                t.variableDeclarator(paramName, requireCall)
+              ]));
+            } else {
+              path.insertBefore(t.expressionStatement(requireCall));
+            }
+          });
+        }
+
+        const factoryReplacement = t.callExpression(t.functionExpression(null, [], factory.body), []);
+        if(name === 'define') {
+          path.replaceWith(
+            t.expressionStatement(
+              t.assignmentExpression(
+                '=',
+                t.memberExpression(t.identifier('module'), t.identifier('exports')),
+                factoryReplacement
+              )
             )
-          )
-        );
-      } else {
-        path.replaceWith(factoryReplacement);
+          );
+        } else {
+          path.replaceWith(factoryReplacement);
+        }
       }
     }
-  }
-});
+  };
+};
