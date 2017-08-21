ec68263456c29700403847030f78cfeb63a09aac
Michael Rose <michael_rose@outlook.com>
2017-08-21 03:56:02 -0400

    Move custom matchers to setup file

--- tests/custom-matchers.js
+++ tests/custom-matchers.js
@@ -2,38 +2,34 @@
 
 const babel = require('babel-core');
 
+const transformAmdToCommonJS = (code) => {
+  return babel.transform(code, { plugins: ['./index'], babelrc: false }).code;
+};
+
+const transformTrivial = (code) => {
+  return babel.transform(code).code;
+};
+
+const removeBlankLines = (string) => {
+  return string.split('\n').filter(line => !!line.trim().length).join('\n');
+};
+
 const customMatchers = {
-  toBeTransformedTo: () => {
-    const transformAmdToCommonJS = (code) => {
-      return babel.transform(code, { plugins: ['./index'], babelrc: false }).code;
+  toBeTransformedTo(actual, expected) {
+    const transformed = transformAmdToCommonJS(actual);
+    actual = removeBlankLines(transformTrivial(actual));
+    expected = removeBlankLines(transformTrivial(expected));
+    const result = {
+      pass: removeBlankLines(transformed) === expected
     };
-
-    const transformTrivial = (code) => {
-      return babel.transform(code).code;
-    };
-
-    const removeBlankLines = (string) => {
-      return string.split('\n').filter(line => !!line.trim().length).join('\n');
-    };
-
-    return {
-      compare(actual, expected) {
-        const transformed = transformAmdToCommonJS(actual);
-        actual = removeBlankLines(transformTrivial(actual));
-        expected = removeBlankLines(transformTrivial(expected));
-        const result = {
-          pass: removeBlankLines(transformed) === expected
-        };
-        if(result.pass) {
-          result.message = `Expected\n\n${actual}\n\nnot to be transformed ` +
-            `to\n\n${expected}\n\nbut instead they were the same.\n`;
-        } else {
-          result.message = `Expected\n\n${actual}\n\nto be transformed ` +
-            `to\n\n${expected}\n\nbut instead got\n\n${transformed}\n`;
-        }
-        return result;
-      }
-    };
+    if(result.pass) {
+      result.message = `Expected\n\n${actual}\n\nnot to be transformed ` +
+        `to\n\n${expected}\n\nbut instead they were the same.\n`;
+    } else {
+      result.message = `Expected\n\n${actual}\n\nto be transformed ` +
+        `to\n\n${expected}\n\nbut instead got\n\n${transformed}\n`;
+    }
+    return result;
   }
 };
 
---
+++ tests/setup.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const customMatchers = require('./custom-matchers');
+
+expect.extend(customMatchers);
