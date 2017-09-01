fc94b4a217eeab7fa98417e3c75b1d075db8a2f7
Michael Rose <michael_rose@outlook.com>
2017-09-01 17:20:30 -0400

    Run prettier

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const EXPORTS = 'exports';
 const DEFINE = 'define';
 
 module.exports = ({ types: t }) => {
-  const decodeDefineArguments = (argNodes) => {
-    if(argNodes.length === 1) {
+  const decodeDefineArguments = argNodes => {
+    if (argNodes.length === 1) {
       return { factory: argNodes[0] };
-    } else if(argNodes.length === 2) {
+    } else if (argNodes.length === 2) {
       const decodedArgs = { factory: argNodes[1] };
-      if(t.isArrayExpression(argNodes[0])) {
+      if (t.isArrayExpression(argNodes[0])) {
         decodedArgs.dependencyList = argNodes[0];
       }
       return decodedArgs;
@@ -20,8 +20,8 @@ module.exports = ({ types: t }) => {
     }
   };
 
-  const decodeRequireArguments = (argNodes) => {
-    if(argNodes.length === 1) {
+  const decodeRequireArguments = argNodes => {
+    if (argNodes.length === 1) {
       return { dependencyList: argNodes[0] };
     } else {
       return { dependencyList: argNodes[0], factory: argNodes[1] };
@@ -33,13 +33,11 @@ module.exports = ({ types: t }) => {
     [REQUIRE]: decodeRequireArguments
   };
 
-  const createModuleExportsAssignmentExpression = (value) => {
+  const createModuleExportsAssignmentExpression = value => {
     return t.expressionStatement(
       t.assignmentExpression(
         '=',
-        t.memberExpression(
-          t.identifier(MODULE), t.identifier(EXPORTS)
-        ),
+        t.memberExpression(t.identifier(MODULE), t.identifier(EXPORTS)),
         value
       )
     );
@@ -47,16 +45,20 @@ module.exports = ({ types: t }) => {
 
   const createRequireExpression = (dependencyNode, variableName) => {
     const requireCall = t.callExpression(t.identifier(REQUIRE), [dependencyNode]);
-    if(variableName) {
+    if (variableName) {
       return t.variableDeclaration('var', [t.variableDeclarator(variableName, requireCall)]);
     } else {
       return t.expressionStatement(requireCall);
     }
   };
 
-  const isModuleOrExportsInDependencyList = (dependencyList) => {
-    return dependencyList && dependencyList.elements.some(element =>
-      t.isStringLiteral(element) && (element.value === MODULE || element.value === EXPORTS)
+  const isModuleOrExportsInDependencyList = dependencyList => {
+    return (
+      dependencyList &&
+      dependencyList.elements.some(
+        element =>
+          t.isStringLiteral(element) && (element.value === MODULE || element.value === EXPORTS)
+      )
     );
   };
 
@@ -65,8 +67,10 @@ module.exports = ({ types: t }) => {
   };
 
   const isModuleOrExportsInjected = (dependencyList, factoryArity) => {
-    return isModuleOrExportsInDependencyList(dependencyList) ||
-      isSimplifiedCommonJSWrapperWithModuleOrExports(dependencyList, factoryArity);
+    return (
+      isModuleOrExportsInDependencyList(dependencyList) ||
+      isSimplifiedCommonJSWrapperWithModuleOrExports(dependencyList, factoryArity)
+    );
   };
 
   // Simple version of zip that only pairs elements until the end of the first array
@@ -79,63 +83,70 @@ module.exports = ({ types: t }) => {
       ExpressionStatement(path) {
         const { node, parent } = path;
 
-        if(!t.isCallExpression(node.expression)) return;
+        if (!t.isCallExpression(node.expression)) return;
 
         const { name } = node.expression.callee;
         const isDefineCall = name === DEFINE;
 
-        if(isDefineCall && !t.isProgram(parent)) return;
+        if (isDefineCall && !t.isProgram(parent)) return;
 
         const argumentDecoder = argumentDecoders[name];
 
-        if(!argumentDecoder) return;
+        if (!argumentDecoder) return;
 
         const { dependencyList, factory } = argumentDecoder(node.expression.arguments);
 
-        if(!t.isArrayExpression(dependencyList) && !factory) return;
+        if (!t.isArrayExpression(dependencyList) && !factory) return;
 
         const isFunctionFactory = t.isFunctionExpression(factory);
         const requireExpressions = [];
         // Order is important here for the simplified commonjs wrapper
         const keywords = [REQUIRE, EXPORTS, MODULE];
 
-        if(dependencyList) {
+        if (dependencyList) {
           const dependencyParameterPairs = zip(
             dependencyList.elements,
             isFunctionFactory ? factory.params : []
           );
 
-          const explicitRequires = dependencyParameterPairs.filter(([dependency]) => {
-            return !t.isStringLiteral(dependency) || keywords.indexOf(dependency.value) === -1;
-          }).map(([dependency, paramName]) => {
-            return createRequireExpression(dependency, paramName);
-          });
+          const explicitRequires = dependencyParameterPairs
+            .filter(([dependency]) => {
+              return !t.isStringLiteral(dependency) || keywords.indexOf(dependency.value) === -1;
+            })
+            .map(([dependency, paramName]) => {
+              return createRequireExpression(dependency, paramName);
+            });
 
           requireExpressions.push(...explicitRequires);
         }
 
-        if(isFunctionFactory) {
+        if (isFunctionFactory) {
           const factoryArity = factory.params.length;
-          let replacementFuncExpr = t.functionExpression(null, [], t.blockStatement(
-            requireExpressions.concat(factory.body.body)
-          ));
+          let replacementFuncExpr = t.functionExpression(
+            null,
+            [],
+            t.blockStatement(requireExpressions.concat(factory.body.body))
+          );
           let replacementCallExprParams = [];
 
           // https://github.com/requirejs/requirejs/wiki/differences-between-the-simplified-commonjs-wrapper-and-standard-amd-define
           const isSimplifiedCommonJSWrapper = !dependencyList && factoryArity > 0;
-          if(isSimplifiedCommonJSWrapper) {
+          if (isSimplifiedCommonJSWrapper) {
             replacementFuncExpr = factory;
             replacementCallExprParams = keywords.slice(0, factoryArity).map(a => t.identifier(a));
           }
 
-          const factoryReplacement = t.callExpression(replacementFuncExpr, replacementCallExprParams);
+          const factoryReplacement = t.callExpression(
+            replacementFuncExpr,
+            replacementCallExprParams
+          );
 
-          if(isDefineCall && !isModuleOrExportsInjected(dependencyList, factoryArity)) {
+          if (isDefineCall && !isModuleOrExportsInjected(dependencyList, factoryArity)) {
             path.replaceWith(createModuleExportsAssignmentExpression(factoryReplacement));
           } else {
             path.replaceWith(factoryReplacement);
           }
-        } else if(factory && isDefineCall) {
+        } else if (factory && isDefineCall) {
           const exportExpression = createModuleExportsAssignmentExpression(factory);
           const nodes = requireExpressions.concat(exportExpression);
           path.replaceWithMultiple(nodes);
