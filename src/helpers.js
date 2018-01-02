da39b5540a1746488c1e2fd185f012a91a038030
Michael Rose <michael_rose@outlook.com>
2018-01-02 04:03:45 -0500

    Inject babel types object into all transformers

--- src/helpers.js
+++ src/helpers.js
@@ -2,53 +2,49 @@
 
 const { MODULE, EXPORTS, REQUIRE } = require('./constants');
 
-// A factory function is exported in order to inject the same babel-types object
-// being used by the plugin itself
-module.exports = ({ types: t }) => {
-  const createModuleExportsAssignmentExpression = value => {
-    return t.expressionStatement(
-      t.assignmentExpression(
-        '=',
-        t.memberExpression(t.identifier(MODULE), t.identifier(EXPORTS)),
-        value
+const createModuleExportsAssignmentExpression = (t, value) => {
+  return t.expressionStatement(
+    t.assignmentExpression(
+      '=',
+      t.memberExpression(t.identifier(MODULE), t.identifier(EXPORTS)),
+      value
+    )
+  );
+};
+
+const createModuleExportsResultCheck = (t, value, identifier) => {
+  return [
+    t.variableDeclaration('var', [t.variableDeclarator(identifier, value)]),
+    t.expressionStatement(
+      t.logicalExpression(
+        '&&',
+        t.binaryExpression(
+          '!==',
+          t.unaryExpression('typeof', identifier),
+          t.stringLiteral('undefined')
+        ),
+        createModuleExportsAssignmentExpression(t, identifier).expression
       )
-    );
-  };
+    )
+  ];
+};
 
-  const createModuleExportsResultCheck = (value, identifier) => {
-    return [
-      t.variableDeclaration('var', [t.variableDeclarator(identifier, value)]),
-      t.expressionStatement(
-        t.logicalExpression(
-          '&&',
-          t.binaryExpression(
-            '!==',
-            t.unaryExpression('typeof', identifier),
-            t.stringLiteral('undefined')
-          ),
-          createModuleExportsAssignmentExpression(identifier).expression
-        )
-      )
-    ];
-  };
+const createRequireExpression = (t, dependencyNode, variableName) => {
+  const requireCall = t.callExpression(t.identifier(REQUIRE), [dependencyNode]);
+  if (variableName) {
+    return t.variableDeclaration('var', [t.variableDeclarator(variableName, requireCall)]);
+  } else {
+    return t.expressionStatement(requireCall);
+  }
+};
 
-  const createRequireExpression = (dependencyNode, variableName) => {
-    const requireCall = t.callExpression(t.identifier(REQUIRE), [dependencyNode]);
-    if (variableName) {
-      return t.variableDeclaration('var', [t.variableDeclarator(variableName, requireCall)]);
-    } else {
-      return t.expressionStatement(requireCall);
-    }
-  };
+const getUniqueIdentifier = (t, scope, name) => {
+  return scope.hasOwnBinding(name) ? scope.generateUidIdentifier(name) : t.identifier(name);
+};
 
-  const getUniqueIdentifier = (scope, name) => {
-    return scope.hasOwnBinding(name) ? scope.generateUidIdentifier(name) : t.identifier(name);
-  };
-
-  return {
-    createModuleExportsAssignmentExpression,
-    createModuleExportsResultCheck,
-    createRequireExpression,
-    getUniqueIdentifier
-  };
+module.exports = {
+  createModuleExportsAssignmentExpression,
+  createModuleExportsResultCheck,
+  createRequireExpression,
+  getUniqueIdentifier
 };
--- src/index.js
+++ src/index.js
@@ -1,209 +1,208 @@
 'use strict';
 
 const { REQUIRE, MODULE, EXPORTS, DEFINE, AMD_DEFINE_RESULT } = require('./constants');
-const createHelpers = require('./helpers');
+const {
+  createRequireExpression,
+  createModuleExportsAssignmentExpression,
+  createModuleExportsResultCheck,
+  getUniqueIdentifier
+} = require('./helpers');
 
-module.exports = ({ types: t }) => {
-  const {
-    createRequireExpression,
-    createModuleExportsAssignmentExpression,
-    createModuleExportsResultCheck,
-    getUniqueIdentifier
-  } = createHelpers({ types: t });
+// Simple version of zip that only pairs elements until the end of the first array
+const zip = (array1, array2) => {
+  return array1.map((element, index) => [element, array2[index]]);
+};
 
-  // Simple version of zip that only pairs elements until the end of the first array
-  const zip = (array1, array2) => {
-    return array1.map((element, index) => [element, array2[index]]);
-  };
+// Order is important here for the simplified commonjs wrapper
+const keywords = [REQUIRE, EXPORTS, MODULE];
 
-  // Order is important here for the simplified commonjs wrapper
-  const keywords = [REQUIRE, EXPORTS, MODULE];
-
-  class AMDExpressionTransformer {
-    constructor(path) {
-      this.path = path;
-    }
-
-    isTransformableAMDExpression() {
-      return t.isArrayExpression(this.getDependencyList()) || this.getFactory();
-    }
-
-    getArguments() {
-      return this.path.node.expression.arguments;
-    }
-
-    getDependencyList() {}
-
-    getFactory() {}
-
-    hasFunctionFactory() {
-      return t.isFunctionExpression(this.getFactory());
-    }
-
-    getCommonJSRequireExpressions() {
-      const dependencyList = this.getDependencyList();
-
-      if (dependencyList) {
-        const dependencyParameterPairs = zip(
-          dependencyList.elements,
-          this.hasFunctionFactory() ? this.getFactory().params : []
-        );
-
-        return dependencyParameterPairs
-          .filter(([dependency]) => {
-            return !t.isStringLiteral(dependency) || !keywords.includes(dependency.value);
-          })
-          .map(([dependency, paramName]) => {
-            return createRequireExpression(dependency, paramName);
-          });
-      }
-
-      return [];
-    }
-
-    // eslint-disable-next-line no-unused-vars
-    processFunctionFactoryReplacement(factoryReplacement) {}
-
-    getFactoryArity() {
-      return this.getFactory().params.length;
-    }
-
-    getFunctionFactoryReplacement() {
-      const factory = this.getFactory();
-      const factoryArity = this.getFactoryArity();
-      let replacementFuncExpr = t.functionExpression(
-        null,
-        [],
-        t.blockStatement(this.getCommonJSRequireExpressions().concat(factory.body.body))
-      );
-      let replacementCallExprParams = [];
-
-      if (this.isSimplifiedCommonJSWrapper()) {
-        replacementFuncExpr = factory;
-        replacementCallExprParams = keywords.slice(0, factoryArity).map(a => t.identifier(a));
-      }
-
-      return this.processFunctionFactoryReplacement(
-        t.callExpression(replacementFuncExpr, replacementCallExprParams)
-      );
-    }
-
-    getNonFunctionFactoryReplacement() {}
-
-    getTransformationToCommonJS() {
-      if (this.hasFunctionFactory()) {
-        return this.getFunctionFactoryReplacement();
-      } else if (this.getFactory()) {
-        return this.getNonFunctionFactoryReplacement();
-      } else {
-        return this.getCommonJSRequireExpressions();
-      }
-    }
-
-    isModuleOrExportsInDependencyList() {
-      const dependencyList = this.getDependencyList();
-      return (
-        dependencyList &&
-        dependencyList.elements.some(
-          element =>
-            t.isStringLiteral(element) && (element.value === MODULE || element.value === EXPORTS)
-        )
-      );
-    }
-
-    isSimplifiedCommonJSWrapper() {
-      return !this.getDependencyList() && this.getFactoryArity() > 0;
-    }
-
-    isSimplifiedCommonJSWrapperWithModuleOrExports() {
-      return this.isSimplifiedCommonJSWrapper() && this.getFactoryArity() > 1;
-    }
-
-    isModuleOrExportsInjected() {
-      return (
-        this.isModuleOrExportsInDependencyList() ||
-        this.isSimplifiedCommonJSWrapperWithModuleOrExports()
-      );
-    }
-
-    static createExpressionTransformer(path) {
-      const decoders = {
-        [DEFINE]: DefineExpressionTransformer,
-        [REQUIRE]: RequireExpressionTransformer
-      };
-      const name = t.isCallExpression(path.node.expression) && path.node.expression.callee.name;
-      return new (decoders[name] || InvalidAMDExpressionTransformer)(path);
-    }
+class AMDExpressionTransformer {
+  constructor(t, path) {
+    this.t = t;
+    this.path = path;
   }
 
-  class RequireExpressionTransformer extends AMDExpressionTransformer {
-    getDependencyList() {
-      return this.getArguments()[0];
+  isTransformableAMDExpression() {
+    return this.t.isArrayExpression(this.getDependencyList()) || this.getFactory();
+  }
+
+  getArguments() {
+    return this.path.node.expression.arguments;
+  }
+
+  getDependencyList() {}
+
+  getFactory() {}
+
+  hasFunctionFactory() {
+    return this.t.isFunctionExpression(this.getFactory());
+  }
+
+  getCommonJSRequireExpressions() {
+    const dependencyList = this.getDependencyList();
+
+    if (dependencyList) {
+      const dependencyParameterPairs = zip(
+        dependencyList.elements,
+        this.hasFunctionFactory() ? this.getFactory().params : []
+      );
+
+      return dependencyParameterPairs
+        .filter(([dependency]) => {
+          return !this.t.isStringLiteral(dependency) || !keywords.includes(dependency.value);
+        })
+        .map(([dependency, paramName]) => {
+          return createRequireExpression(this.t, dependency, paramName);
+        });
     }
 
-    getFactory() {
-      return this.getArguments()[1];
+    return [];
+  }
+
+  // eslint-disable-next-line no-unused-vars
+  processFunctionFactoryReplacement(factoryReplacement) {}
+
+  getFactoryArity() {
+    return this.getFactory().params.length;
+  }
+
+  getFunctionFactoryReplacement() {
+    const factory = this.getFactory();
+    const factoryArity = this.getFactoryArity();
+    let replacementFuncExpr = this.t.functionExpression(
+      null,
+      [],
+      this.t.blockStatement(this.getCommonJSRequireExpressions().concat(factory.body.body))
+    );
+    let replacementCallExprParams = [];
+
+    if (this.isSimplifiedCommonJSWrapper()) {
+      replacementFuncExpr = factory;
+      replacementCallExprParams = keywords.slice(0, factoryArity).map(a => this.t.identifier(a));
     }
 
-    processFunctionFactoryReplacement(factoryReplacement) {
-      return [t.expressionStatement(factoryReplacement)];
-    }
+    return this.processFunctionFactoryReplacement(
+      this.t.callExpression(replacementFuncExpr, replacementCallExprParams)
+    );
+  }
 
-    getNonFunctionFactoryReplacement() {
+  getNonFunctionFactoryReplacement() {}
+
+  getTransformationToCommonJS() {
+    if (this.hasFunctionFactory()) {
+      return this.getFunctionFactoryReplacement();
+    } else if (this.getFactory()) {
+      return this.getNonFunctionFactoryReplacement();
+    } else {
       return this.getCommonJSRequireExpressions();
     }
   }
 
-  class DefineExpressionTransformer extends AMDExpressionTransformer {
-    isTransformableAMDExpression() {
-      return super.isTransformableAMDExpression() && t.isProgram(this.path.parent);
-    }
+  isModuleOrExportsInDependencyList() {
+    const dependencyList = this.getDependencyList();
+    return (
+      dependencyList &&
+      dependencyList.elements.some(
+        element =>
+          this.t.isStringLiteral(element) && (element.value === MODULE || element.value === EXPORTS)
+      )
+    );
+  }
 
-    getDependencyList() {
-      const args = this.getArguments();
-      if (args.length === 2) {
-        if (t.isArrayExpression(args[0])) {
-          return args[0];
-        }
-      } else if (args.length !== 1) {
-        return args[1];
-      }
-    }
+  isSimplifiedCommonJSWrapper() {
+    return !this.getDependencyList() && this.getFactoryArity() > 0;
+  }
 
-    getFactory() {
-      const args = this.getArguments();
-      if (args.length === 1) {
+  isSimplifiedCommonJSWrapperWithModuleOrExports() {
+    return this.isSimplifiedCommonJSWrapper() && this.getFactoryArity() > 1;
+  }
+
+  isModuleOrExportsInjected() {
+    return (
+      this.isModuleOrExportsInDependencyList() ||
+      this.isSimplifiedCommonJSWrapperWithModuleOrExports()
+    );
+  }
+}
+
+class RequireExpressionTransformer extends AMDExpressionTransformer {
+  getDependencyList() {
+    return this.getArguments()[0];
+  }
+
+  getFactory() {
+    return this.getArguments()[1];
+  }
+
+  processFunctionFactoryReplacement(factoryReplacement) {
+    return [this.t.expressionStatement(factoryReplacement)];
+  }
+
+  getNonFunctionFactoryReplacement() {
+    return this.getCommonJSRequireExpressions();
+  }
+}
+
+class DefineExpressionTransformer extends AMDExpressionTransformer {
+  isTransformableAMDExpression() {
+    return super.isTransformableAMDExpression() && this.t.isProgram(this.path.parent);
+  }
+
+  getDependencyList() {
+    const args = this.getArguments();
+    if (args.length === 2) {
+      if (this.t.isArrayExpression(args[0])) {
         return args[0];
-      } else if (args.length === 2) {
-        return args[1];
-      } else {
-        return args[2];
       }
-    }
-
-    processFunctionFactoryReplacement(factoryReplacement) {
-      if (!this.isModuleOrExportsInjected()) {
-        return [createModuleExportsAssignmentExpression(factoryReplacement)];
-      } else {
-        const resultCheckIdentifier = getUniqueIdentifier(this.path.scope, AMD_DEFINE_RESULT);
-        return createModuleExportsResultCheck(factoryReplacement, resultCheckIdentifier);
-      }
-    }
-
-    getNonFunctionFactoryReplacement() {
-      const exportExpression = createModuleExportsAssignmentExpression(this.getFactory());
-      return this.getCommonJSRequireExpressions().concat(exportExpression);
+    } else if (args.length !== 1) {
+      return args[1];
     }
   }
 
-  class InvalidAMDExpressionTransformer extends AMDExpressionTransformer {
-    isTransformableAMDExpression() {
-      return false;
+  getFactory() {
+    const args = this.getArguments();
+    if (args.length === 1) {
+      return args[0];
+    } else if (args.length === 2) {
+      return args[1];
+    } else {
+      return args[2];
     }
   }
 
+  processFunctionFactoryReplacement(factoryReplacement) {
+    if (!this.isModuleOrExportsInjected()) {
+      return [createModuleExportsAssignmentExpression(this.t, factoryReplacement)];
+    } else {
+      const resultCheckIdentifier = getUniqueIdentifier(this.t, this.path.scope, AMD_DEFINE_RESULT);
+      return createModuleExportsResultCheck(this.t, factoryReplacement, resultCheckIdentifier);
+    }
+  }
+
+  getNonFunctionFactoryReplacement() {
+    const exportExpression = createModuleExportsAssignmentExpression(this.t, this.getFactory());
+    return this.getCommonJSRequireExpressions().concat(exportExpression);
+  }
+}
+
+class InvalidAMDExpressionTransformer extends AMDExpressionTransformer {
+  isTransformableAMDExpression() {
+    return false;
+  }
+}
+
+const createExpressionTransformer = (t, path) => {
+  const decoders = {
+    [DEFINE]: DefineExpressionTransformer,
+    [REQUIRE]: RequireExpressionTransformer
+  };
+  const name = t.isCallExpression(path.node.expression) && path.node.expression.callee.name;
+  return new (decoders[name] || InvalidAMDExpressionTransformer)(t, path);
+};
+
+module.exports = ({ types: t }) => {
   const ExpressionStatement = path => {
-    const transformer = AMDExpressionTransformer.createExpressionTransformer(path);
+    const transformer = createExpressionTransformer(t, path);
 
     if (!transformer.isTransformableAMDExpression()) return;
 
