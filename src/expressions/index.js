5e087aa6412dfe8efb80524c4cec5036fb91b3d4
Michael Rose <michael_rose@outlook.com>
2018-01-02 06:15:52 -0500

    Use consistent naming

--- src/expressions/index.js
+++ src/expressions/index.js
@@ -4,14 +4,14 @@ const { DEFINE, REQUIRE } = require('../constants');
 
 const DefineExpression = require('./DefineExpression');
 const RequireExpression = require('./RequireExpression');
-const NullAMDExpression = require('./NullExpression');
+const NullExpression = require('./NullExpression');
 
 const getExpressionClass = name => {
   const decoders = {
     [DEFINE]: DefineExpression,
     [REQUIRE]: RequireExpression
   };
-  return decoders[name] || NullAMDExpression;
+  return decoders[name] || NullExpression;
 };
 
 module.exports = getExpressionClass;
